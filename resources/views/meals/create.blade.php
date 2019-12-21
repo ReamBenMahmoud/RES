3aef04524803a3a9b7477cf2aa8d6b46bce35ccd
Ream Ben Mahmoud <remomahmoud4@gmail.com>
2019-12-21 15:27:04 +0200

    Update

--- resources/views/meals/create.blade.php
+++ resources/views/meals/create.blade.php
@@ -1,12 +1,18 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+<style> 
+#fonts {
+  font-family: 'Tajawal', sans-serif;
+}
+</style> 
 <div class="container">
-    <h2>Add New Meal</h2>
+    <h2 id="fonts" class="ml-3">إضافة وجبة جديدة</h2>
 
-    <form action="{{ url('/meals/store') }}" method="post" enctype="multipart/form-data">
+    <form class="form-group col-md-4 col-form-label text-md-right" action="{{ url('/meals/store') }}" method="post" enctype="multipart/form-data">
         @csrf
 
         <div class="row form-group">
-            <label class="col-2 col-form-label">Name</label>
+            <label  id="fonts" class="col-2 col-form-label">الإسم</label>
 
             <div class="col-10">
                 <input type="text" name="name" class="form-control">
@@ -14,7 +20,7 @@
         </div>
 
         <div class="row form-group">
-            <label class="col-2 col-form-label">Price</label>
+            <label  id="fonts" class="col-2 col-form-label">السعر</label>
 
             <div class="col-10">
                 <input type="number" name="price" class="form-control">
@@ -22,7 +28,7 @@
         </div>
 
         <div class="row form-group">
-            <label class="col-2 col-form-label">Description</label>
+            <label id="fonts" class="col-2.5 col-form-label">التفاصيل</label>
 
             <div class="col-10">
                 <textarea name="details" class="form-control"></textarea>
@@ -30,9 +36,9 @@
         </div>
 
         <div class="row form-group">
-            <label class="col-2 col-form-label">Category</label>
+            <label id="fonts" class="col-2 col-form-label">الصنف</label>
 
-            <div class="col-10">
+            <div id="fonts" class="col-10">
                 <select name="category" class="form-control">
                     @foreach ($categories as $category)
                         <option value="{{ $category->id }}"> {{ $category->name }} </option>
@@ -40,13 +46,13 @@
                 </select>
             </div>
         </div>
-  <div class="col-10">
+  <div id="fonts" class="col-10">
   صورة
                 <input type="file" name="image" class="form-control">
             </div>
         </div>
         <div class="row form-group">
-            <input type="submit" value="Add" class="btn btn-primary offset-2">
+            <input type="submit" value="Add" class="btn btn-success offset-2">
         </div>
     </form>
 </div>
